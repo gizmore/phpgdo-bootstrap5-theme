b29848e6d8b90a7868846399a9a8b11d384c527e
gizmore <gizmore@wechall.net>
2022-05-21 21:54:19 +0200

    7.0.1

--- thm/bs5/UI/page.php
+++ thm/bs5/UI/page.php
@@ -1,17 +1,14 @@
-<?php /** @var $page \GDO\UI\GDT_Page **/
+<?php
+namespace GDO\Bootstrap5Theme\thm\bs5\UI;
 use GDO\Core\Website;
-use GDO\Javascript\Javascript;
-use GDO\User\GDO_User;
+use GDO\Core\Javascript;
 use GDO\UI\GDT_Page;
 use GDO\UI\GDT_Loading;
-use GDO\Profile\GDT_ProfileLink;
-use GDO\Javascript\Module_Javascript;
 use GDO\Language\Trans;
 /** @var $page GDT_Page **/
-$user = GDO_User::current();
 ?>
 <!DOCTYPE html>
-<html lang="<?=Trans::$ISO?>">
+<html class="nojs" lang="<?=Trans::$ISO?>">
     <head>
      	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -20,13 +17,13 @@ $user = GDO_User::current();
 		<?=Website::displayMeta()?>
 		<?=Website::displayLink()?>
     </head>
-    <body class="nojs">
+    <body>
         <div class="d-flex" id="wrapper">
             <!-- Sidebar-->
             <div class="border-end bg-white" id="sidebar-wrapper">
                 <ul id="leftnav" class="list-group list-group-flush">
-<?php foreach (GDT_Page::$INSTANCE->leftNav->getFields() as $gdt) : ?>                
-                    <li class="list-group-item list-group-item-action"><?=$gdt->renderSidebar()?></li>
+<?php foreach ($page->leftBar()->getFields() as $gdt) : ?>
+                    <li class="list-group-item list-group-item-action"><?=$gdt->renderHTML()?></li>
 <?php endforeach; ?>
                 </ul>
             </div>
@@ -39,24 +36,24 @@ $user = GDO_User::current();
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-<?php foreach (GDT_Page::$INSTANCE->rightNav->getFields() as $gdt) : ?>                
-                                <li class="nav-item"><span class="nav-link"><?=$gdt->renderSidebar()?></span></li>
+<?php foreach ($page->rightBar()->getFields() as $gdt) : ?>
+                                <li class="nav-item"><span class="nav-link"><?=$gdt->renderHTML()?></span></li>
 <?php endforeach; ?>                                        
                             </ul>
                         </div>
                     </div>
                 </nav>
                 <!-- topTabs -->
-                <?=$page->topTabs->renderCell()?>
+                <?=$page->topTabs->renderHTML()?>
                 <!-- Page content-->
                 <div id="content-wrap" class="container-fluid">
-                    <?=Website::topResponse()->renderCell()?>
-                    <?=$page->html?>
+                    <?=$page->topBar()->renderHTML()?>
+                    <?=$page->renderGDT()?>
                 </div>
             </div>
         </div>
-        <footer><?=$page->bottomNav->addClass('gdo-footer')->renderCell()?></footer>
-	    <?=GDT_Loading::make()->renderCell()?>
-		<?=Javascript::displayJavascripts(Module_Javascript::instance()->cfgMinifyJS() === 'concat')?>
+        <footer><?=$page->bottomBar()->addClass('gdt-footer')->renderCell()?></footer>
+	    <?=GDT_Loading::make()->renderHTML()?>
+		<?=Javascript::displayJavascripts()?>
     </body>
 </html>
