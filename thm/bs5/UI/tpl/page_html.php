ad9330c4e6cf1f5f1a5bb4ed31551da2a9dbff81
gizmore <gizmore@wechall.net>
2024-01-26 21:16:24 +0100

    'new SEO urls'

--- thm/bs5/UI/tpl/page_html.php
+++ thm/bs5/UI/tpl/page_html.php
@@ -36,7 +36,7 @@ use GDO\UI\GDT_Page;
         <!-- Top navigation-->
         <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
             <div class="container-fluid">
-                <button class="btn btn-primary" id="sidebarToggle"><span class="navbar-toggler-icon"></span></button>
+                <button class="btn btn-primary" id="sidebarToggle"><span class="navbar-toggler-icon" aria-label="<?=t('toggle_sidebar')?>"></span></button>
                 <div class="navbar-brand"><?=$page->topBar()->renderHTML()?></div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
