f4b79f242b30554f82c5106a2cb8ace0ccd6caac
gizmore <gizmore@wechall.net>
2024-03-05 07:32:35 +0100

    'form pack'

--- thm/bs5/UI/tpl/tabs_html.php
+++ thm/bs5/UI/tpl/tabs_html.php
@@ -6,36 +6,34 @@ use GDO\UI\GDT_Tabs;
 /** @var $cell bool * */
 /** @var $field GDT_Tabs * */
 ?>
-<ul class="nav nav-tabs" role="tablist">
+<div class="gdt-tabs">
+  <ul class="nav nav-tabs" role="tablist">
     <?php
 	foreach ($field->getTabs() as $tab) :
     ?>
         <li class="nav-item" role="presentation">
-            <a class="nav-link <?=$tab->active?'active':''?>" id="tab_<?=html($tab->getName())?>" data-bs-toggle="tab" href="#simple-tabpanel-0" role="tab" aria-controls="simple-tabpanel-0" aria-selected="true">Tab 1</a>
+            <a class="nav-link <?=$tab->active?'active':''?>"
+               id="<?=$field->getName()?>_<?=$tab->getName()?>"
+               data-bs-toggle="tab"
+               href="#tab_<?=$tab->getName()?>"
+               role="tab"
+               aria-controls="<?=t('switch_tab', [$tab->renderLabel()])?>"
+               aria-selected="<?=$tab->active?'true':'false'?>"><?=$tab->renderLabel()?></a>
         </li>
     <?php
     endforeach;
     ?>
-    <li class="nav-item" role="presentation">
-        <a class="nav-link active" id="simple-tab-0" data-bs-toggle="tab" href="#simple-tabpanel-0" role="tab" aria-controls="simple-tabpanel-0" aria-selected="true">Tab 1</a>
-    </li>
-    <li class="nav-item" role="presentation">
-        <a class="nav-link" id="simple-tab-1" data-bs-toggle="tab" href="#simple-tabpanel-1" role="tab" aria-controls="simple-tabpanel-1" aria-selected="false">Tab 2</a>
-    </li>
-    <li class="nav-item" role="presentation">
-        <a class="nav-link" id="simple-tab-2" data-bs-toggle="tab" href="#simple-tabpanel-2" role="tab" aria-controls="simple-tabpanel-2" aria-selected="false">Tab 3</a>
-    </li>
-</ul>
-<div class="tab-content pt-5" id="tab-content">
-    <div class="tab-pane active" id="simple-tabpanel-0" role="tabpanel" aria-labelledby="simple-tab-0">Tab 1 selected</div>
-    <div class="tab-pane" id="simple-tabpanel-1" role="tabpanel" aria-labelledby="simple-tab-1">Tab 2 selected</div>
-    <div class="tab-pane" id="simple-tabpanel-2" role="tabpanel" aria-labelledby="simple-tab-2">Tab 3 selected</div>
-</div>
-
-<div class="gdt-tabs">
-	<?php
-	foreach ($field->getTabs() as $tab) :
-		echo $cell ? $tab->renderHTML() : $tab->renderForm();
-	endforeach;
-	?>
+  </ul>
+  <div class="tab-content pt-5" id="tab_content_<?=$field->getName()?>">
+    <?php
+    foreach ($field->getTabs() as $tab) :
+    ?>
+    <div class="tab-pane <?=$tab->active?'active':''?>"
+         id="tab_<?=$tab->getName()?>"
+         role="tabpanel"
+         aria-labelledby="<?=$field->getName()?>_<?=$tab->getName()?>"><?=$tab->render()?></div>
+    <?php
+    endforeach;
+    ?>
+  </div>
 </div>
